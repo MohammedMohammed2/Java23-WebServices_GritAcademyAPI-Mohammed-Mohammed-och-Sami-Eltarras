7f5184aaa46ed29a9f3c3821e17171261a2d706f
sami-elt <139684759+sami-elt@users.noreply.github.com>
2024-03-14 11:28:40 +0100

    .

--- src/main/java/com/gritacademyAPI/studentscourses/StudentsCourses.java
+++ src/main/java/com/gritacademyAPI/studentscourses/StudentsCourses.java
@@ -1,5 +1,7 @@
 package com.gritacademyAPI.studentscourses;
 
+import com.gritacademyAPI.courses.Courses;
+import com.gritacademyAPI.studenter.Students;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,10 +12,18 @@ import java.util.Set;
 @Table(name = "students_courses")
 @Data
 public class StudentsCourses {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToMany
+    @JoinColumn(name = "students_id", referencedColumnName = "id")
+    private Students students;
+
+    @ManyToMany
+    @JoinColumn(name = "courses_id", referencedColumnName = "id")
+    private Courses coursesId;
 
 }
